20d2f16989d23e970240c5713339b5eac21c4c18
Martin Garcia <newluxfero@gmail.com>
2012-05-13 07:05:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name = "pycture-tube",
     packages = ["pycture-tube"],
     platforms = ["POSIX", "Windows"],
     long_description = open(os.path.join(HERE, "README.md"), "r").read(),
-    install_requires=["Twisted", "PIL"],
+    install_requires=["Twisted", "PIL", "zlib"],
     classifiers = [
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
